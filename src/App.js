b01daa986fcacec462f6a71750e62c383eb3a0a0
Shanmugaraj <shanmugaraj.durairajan@rasiinfotech.com>
2024-04-25 17:56:35 +0530

    first phase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './component/home/Home';
+import Header from './component/header/Header';
+import About from './component/about/About';
+import Skills from './component/skills/Skills';
+import Services from './component/services/Services';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+      </main>
+    </>
   );
 }
 
